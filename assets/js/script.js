4c56e1e322194c703dae663b006354f375234d96
RoryMurchison <roryjmurchison@gmail.com>
2022-12-08 21:11:42 +0000

    add event listener for buttons being clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
-const button = document.getElementsByClassName("choice-button");
+const buttons = document.getElementsByClassName("choice-button");
 const resultMsg = document.getElementById("result-message");
 const playOpt = document.getElementById("player-option");
 const cpuOpt = document.getElementById("computer-option");
 
-function runGame() {
+
+for (button of buttons) {
+    button.addEventListener("click", function() {
+        let playerButton = this.getAttribute("data-choice");
+        playGame(playerButton);
+    })
+}
+
+function playGame() {
 
 };
 
@@ -19,6 +27,6 @@ function playerWin() {
 
 };
 
-function playerLose {
+function playerLose() {
 
 };
