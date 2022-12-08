8ecb825dddbe05b7e316f7dc8dc5e8bbc7f20f98
RoryMurchison <roryjmurchison@gmail.com>
2022-12-08 20:57:04 +0000

    add const variables and computer turn function template

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const button = document.getElementsByClassName("choice-button");
+const resultMsg = document.getElementById("result-message");
+const playOpt = document.getElementById("player-option");
+const cpuOpt = document.getElementById("computer-option");
+
+function runGame() {
+
+};
+
+function computerTurn() {
+
+};
+
+function checkWinner() {
+
+};
+
+function playerWin() {
+
+};
+
+function playerLose {
+
+};
