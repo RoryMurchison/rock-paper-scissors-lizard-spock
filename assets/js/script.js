50635400f45b559dbe8a5d48aee3f93f6085c825
RoryMurchison <roryjmurchison@gmail.com>
2022-12-08 22:15:36 +0000

    add code for computer turn function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,21 +8,56 @@ const cpuOpt = document.getElementById("computer-option");
 let wins = document.getElementById("games-won");
 let lost = document.getElementById("games-lost");
 let item;
+let cpuItem;
+let result;
 
 
 /**
  * Adds an event listener looking for any buttons being clicked
  */
 for (button of buttons) {
+
     button.addEventListener("click", function() {
-        item = this.textContent
-        playOpt.textContent = `${item}`;
+        
         let playerButton = this.getAttribute("data-choice");
         playGame(playerButton);
+        item = this.textContent
+        playOpt.textContent = `${item}`;
+        cpuOpt.textContent = `${cpuItem}`;
+
     })
-}
+};
+
+/**
+ * Main game code, using the data choice valuble taken from the button clicked by the user
+ */
+function playGame(playerButton) {
+
+    let cpuItem = computerTurn();
+
+};
 
 function computerTurn() {
+
+    let computerNumber = Math.floor(Math.random() * 5);
+
+    switch(computerNumber){
+        case 0:
+            cpuItem = "Rock";
+            break;
+        case 1:
+            cpuItem = "Paper";
+            break; 
+        case 2:
+            cpuItem = "Scissors";
+            break; 
+        case 3:
+            cpuItem = "Lizard";
+            break; 
+        case 4:
+            cpuItem = "Spock";
+            break; 
+    }
 
 };
 
