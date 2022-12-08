b0d0af2c4ab0954b46e8bc8393f6f929ca145b47
RoryMurchison <roryjmurchison@gmail.com>
2022-12-08 21:34:50 +0000

    added item variable displaying player option

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,26 @@
+/**
+ * Declaring variables
+ */
 const buttons = document.getElementsByClassName("choice-button");
 const resultMsg = document.getElementById("result-message");
 const playOpt = document.getElementById("player-option");
 const cpuOpt = document.getElementById("computer-option");
+let wins = document.getElementById("games-won");
+let lost = document.getElementById("games-lost");
+let item;
 
 
+/**
+ * Adds an event listener looking for any buttons being clicked
+ */
 for (button of buttons) {
     button.addEventListener("click", function() {
+        item = this.textContent
+        playOpt.textContent = `${item}`;
         let playerButton = this.getAttribute("data-choice");
         playGame(playerButton);
     })
 }
-
-function playGame() {
-
-};
 
 function computerTurn() {
 
